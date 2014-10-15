b28d54d834ae6bfe7976fffc21ebad88564c4a78
Gudger_Parvel_WPF <plgudger@fullsail.edu>
2014-10-15 02:58:16 -0400

    Made Clone

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,22 @@
+//Dogs Age
+var DogsName = "Sparky";
+
+var DogsAgeHumanYears = 6; //Age Human Years
+// Yes or No
+var isDog = true;
+
+var DogsAgeDogYears = 42; //Age Dog Years
+
+var isDog = true;
+
+var AgeHuman = 6;
+
+var AgeDog = 42;
+
+var 7 * 6 = 42; //Caluations
+//Print log
+console.log("Hello Professor Garlic");
+console.log(DogsAgeDogYears);
+
+alert(DogsName);
+alert(AgeDog);
