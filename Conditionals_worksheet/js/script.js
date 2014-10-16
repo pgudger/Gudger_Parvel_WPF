b7941996481dfb26d77f5abeecbcddf37c770b8f
Gudger_Parvel_WPF <plgudger@fullsail.edu>
2014-10-16 12:58:07 -0400

    assignments

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,42 @@
+
+ /*
+Parvel Gudger
+WPF 1408 Section 02
+Conditionals worksheet
+10-16-14
+ */
+
+// Last Chance for Gas!
+
+ //Array
+var CarsGasEfficiency = ["City 15mpg", "Highway 46mpg"];
+
+ //console log gas efficiency
+ console.log(CarsGasEfficiency);
+
+ //Gas gauge variable
+ var GasGaugeReading = 0.25;
+
+ //console log Gas Gauge
+ console.log(GasGaugeReading);
+
+ //CarsGasTankCapacity
+
+ var CarsTankCapacityInGallons = 20;
+
+ //console log tank
+ console.log(CarsTankCapacityInGallons);
+
+ //Actual gallons of gas in car
+ var ActualGallonsInTank = GasGaugeReading/CarsTankCapacityInGallons;
+
+ //console log actual gallons
+ console.log(ActualGallonsInTank);
+
+ if (GasGaugeReading === "City 15mpg"){
+
+
+ }else {
+
+     console.log("Time To Get Gas! Last Chance!")
+ }
