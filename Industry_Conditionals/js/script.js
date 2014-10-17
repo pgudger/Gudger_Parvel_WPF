80652de3cffe3dc2c29020d1e0df6d5c5c52bc51
Gudger_Parvel_WPF <plgudger@fullsail.edu>
2014-10-17 12:27:38 -0400

    My industry conditional been up since 8am yesterday. This one is really simple. Sorry.

--- Industry_Conditionals/js/script.js
+++ Industry_Conditionals/js/script.js
@@ -6,4 +6,64 @@ WPF 1408 Section 02
 10-16-14
  */
 
-alert("Testing 1,2,3");
+alert("Hello my name is Parvel");
+ alert("and I am going to school");
+ alert("to become a Web Designer.");
+
+ alert("I've learned lots of new things.");
+ alert("For example I can ask you for you name");
+ alert("Like this.");
+
+ var askingForName = prompt("Please enter your name.");
+
+ console.log(askingForName);
+
+ if (askingForName){
+
+     alert("Hello " + askingForName + " it's very nice to meet you.");
+
+     console.log("Hello " + askingForName + " it's very nice to meet you.")
+
+ }
+
+
+ alert("Pretty cool isn't it?");
+ alert("I can also make it");
+ alert("to where you can't do anything");
+ alert("if you don't at least input something");
+ alert("to my questions unless you reload the page");
+ alert("This time don't put anything in");
+ alert("When you are finished with my little test");
+ alert("just reload the page and answer");
+
+ var shoeSize = prompt("What is your shoe size.");
+
+ console.log(shoeSize);
+
+ if (shoeSize <= 1){
+
+     alert("You have to enter some kind actual shoe size start from the top.");
+
+     for (initialization; condition; increment) {
+         // statements
+     }
+
+
+
+ }else if (shoeSize >=1){
+
+     alert("Sorry to have made you go through that again");
+     alert("but maybe you should have answered my question");
+     alert("I'm just messing with you");
+     alert("Thank you for taking the to time to visit my page");
+     alert("I hope next time you visit I can have");
+     alert("even more amazing things to show you");
+
+ }
+
+
+
+
+
+
+
