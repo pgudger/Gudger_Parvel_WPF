a4de8c912baa8137e3b935378cd8146a69c4b302
Gudger_Parvel_WPF <plgudger@fullsail.edu>
2014-10-17 11:10:18 -0400

    Wacky conditional

--- Wacky_Conditionals/js/script.js
+++ Wacky_Conditionals/js/script.js
@@ -6,4 +6,523 @@ WPF 1408 Section 02
 10-16-14
  */
 
-alert("Testing 1,2,3");
+alert("Hello my name is Parvel. What's yours?");
+
+ var name = prompt("Enter name.");
+
+ console.log(name);
+
+ if (name){
+
+     alert("Hello "  +name+  " Lets play a game.");
+
+ }else{
+
+     alert("Can't play a game together if I don't know your name.");
+     alert("Goodbye");
+
+     for (initialization; condition; increment) {
+
+     }
+ }
+
+ var monthBorn =[1,2,3,4,5,6,7,8,9,10,11,12];
+
+ console.log(monthBorn);
+
+ console.log(monthBorn.length);
+
+ alert("You ready to have fun?");
+
+ var monthBornPrompt = prompt("What's the number for the month you were born.");
+
+ if (monthBornPrompt == 1 ) {
+
+     alert("I fell in love with");
+
+    console.log(monthBornPrompt == 1);
+    console.log("I fell in love with");
+
+
+ }else if (monthBornPrompt == 2){
+
+     alert("I ate");
+
+    console.log(monthBornPrompt == 2);
+    console.log("I ate");
+
+
+ }else if (monthBornPrompt == 3){
+
+     alert("I smacked");
+
+    console.log(monthBornPrompt == 3);
+    console.log("I smacked");
+
+
+ }else if (monthBornPrompt == 4){
+
+     alert("I sang to");
+
+    console.log(monthBornPrompt == 4);
+    console.log("I sang to");
+
+
+ }else if (monthBornPrompt == 5){
+
+     alert("I gave my number to");
+
+    console.log(monthBornPrompt == 5);
+    console.log("I gave my number to");
+
+
+ }else if (monthBornPrompt == 6){
+
+     alert("I murdered");
+
+     console.log(monthBornPrompt == 6);
+     console.log("I murdered");
+
+
+ }else if (monthBornPrompt == 7){
+
+     alert("I shot");
+
+     console.log(monthBornPrompt == 7);
+     console.log("I shot");
+
+
+ }else if (monthBornPrompt == 8){
+
+     alert("I danced with");
+
+     console.log(monthBornPrompt == 8);
+     console.log("I danced with");
+
+
+ }else if (monthBornPrompt == 9){
+
+     alert("I choked on");
+
+     console.log(monthBornPrompt == 9);
+     console.log("I choked on");
+
+
+ }else if (monthBornPrompt == 10){
+
+     alert("I went out with");
+
+     console.log(monthBornPrompt == 10);
+     console.log("I went out with");
+
+
+
+ }else if (monthBornPrompt == 11){
+
+     alert("I kissed");
+
+     console.log(monthBornPrompt == 11);
+     console.log("I kissed");
+
+
+ }else if (monthBornPrompt == 12){
+
+     alert("I hugged");
+
+     console.log(monthBornPrompt == 12);
+     console.log("I hugged");
+
+ }else if (monthBornPrompt >= 0){
+
+     alert("Your not playing along.");
+     alert("Goodbye.");
+
+     for (initialization; condition; increment) {
+         // statements
+     }
+
+ }
+
+ var dayOfTheMonth = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+
+    console.log(dayOfTheMonth);
+    console.log(dayOfTheMonth.length);
+
+
+    alert("Now for the day of the month you where born.");
+
+ var dayOfTheMonthPrompt = prompt("Please input the number of the day you where born on.");
+
+ if (dayOfTheMonthPrompt == 1){
+
+     alert("a homeless guy");
+
+     console.log(dayOfTheMonthPrompt == 1);
+     console.log("a homeless guy");
+
+ }else if (dayOfTheMonthPrompt == 2){
+
+     alert("your mom");
+
+     console.log(dayOfTheMonthPrompt == 2);
+     console.log("your mom");
+
+ }else if (dayOfTheMonthPrompt == 3){
+
+     alert("a banana");
+
+     console.log(dayOfTheMonthPrompt == 3);
+     console.log("a banana");
+
+ }else if (dayOfTheMonthPrompt == 4){
+
+     alert("a fork");
+
+     console.log(dayOfTheMonthPrompt == 4);
+     console.log("a fork");
+
+ }else if (dayOfTheMonthPrompt == 5){
+
+     alert("a chicken");
+
+     console.log(dayOfTheMonthPrompt == 5);
+     console.log("a chicken");
+
+ }else if (dayOfTheMonthPrompt == 6){
+
+     alert("a gangster");
+
+     console.log(dayOfTheMonthPrompt == 6);
+     console.log("a gangster");
+
+ }else if (dayOfTheMonthPrompt == 7){
+
+     alert("a ninja");
+
+     console.log(dayOfTheMonthPrompt == 7);
+     console.log("a ninja");
+
+ }else if (dayOfTheMonthPrompt == 8){
+
+     alert("an ipod");
+
+     console.log(dayOfTheMonthPrompt == 8);
+     console.log("an ipod");
+
+ }else if (dayOfTheMonthPrompt == 9){
+
+     alert("my best friend");
+
+     console.log(dayOfTheMonthPrompt == 9);
+     console.log("my best friend");
+
+ }else if (dayOfTheMonthPrompt == 10){
+
+     alert("a goat");
+
+     console.log(dayOfTheMonthPrompt == 10);
+     console.log("a goat");
+
+ }else if (dayOfTheMonthPrompt == 11){
+
+     alert("my dog");
+
+     console.log(dayOfTheMonthPrompt == 11);
+     console.log("my dog");
+
+ }else if (dayOfTheMonthPrompt == 12){
+
+     alert("my dad");
+
+     console.log(dayOfTheMonthPrompt == 12);
+     console.log("my dad");
+
+ }else if (dayOfTheMonthPrompt == 13){
+
+     alert("the computer");
+
+     console.log(dayOfTheMonthPrompt == 13);
+     console.log("the computer");
+
+ }else if (dayOfTheMonthPrompt == 14){
+
+     alert("a football player");
+
+     console.log(dayOfTheMonthPrompt == 14);
+     console.log("a football player");
+
+ }else if (dayOfTheMonthPrompt == 15){
+
+     alert("my neighbor");
+
+     console.log(dayOfTheMonthPrompt == 15);
+     console.log("my neighbor");
+
+ }else if (dayOfTheMonthPrompt == 16){
+
+     alert("myself");
+
+     console.log(dayOfTheMonthPrompt == 16);
+     console.log("myself");
+
+ }else if (dayOfTheMonthPrompt == 17){
+
+     alert("a soda");
+
+     console.log(dayOfTheMonthPrompt == 17);
+     console.log("a soda");
+
+ }else if (dayOfTheMonthPrompt == 18){
+
+     alert("a llama");
+
+     console.log(dayOfTheMonthPrompt == 18);
+     console.log("a llama");
+
+ }else if (dayOfTheMonthPrompt == 19){
+
+     alert("a pickle");
+
+     console.log(dayOfTheMonthPrompt == 19);
+     console.log("a pickle");
+
+ }else if (dayOfTheMonthPrompt == 20){
+
+     alert("a stuffed animal");
+
+     console.log(dayOfTheMonthPrompt == 20);
+     console.log("a stuffed animal");
+
+ }else if (dayOfTheMonthPrompt == 21){
+
+     alert("a weirdo");
+
+     console.log(dayOfTheMonthPrompt == 21);
+     console.log("a weirdo");
+
+ }else if (dayOfTheMonthPrompt == 22){
+
+     alert("a sock");
+
+     console.log(dayOfTheMonthPrompt == 22);
+     console.log("a sock");
+
+ }else if (dayOfTheMonthPrompt == 23){
+
+     alert("a doctor");
+
+     console.log(dayOfTheMonthPrompt == 23);
+     console.log("a doctor");
+
+ }else if (dayOfTheMonthPrompt == 24){
+
+     alert("my psychiatrist");
+
+     console.log(dayOfTheMonthPrompt == 24);
+     console.log("my psychiatrist");
+
+ }else if (dayOfTheMonthPrompt == 25){
+
+     alert("a police man");
+
+     console.log(dayOfTheMonthPrompt == 25);
+     console.log("a police man");
+
+ }else if (dayOfTheMonthPrompt == 26){
+
+     alert("my brother");
+
+     console.log(dayOfTheMonthPrompt == 26);
+     console.log("my brother");
+
+ }else if (dayOfTheMonthPrompt == 27){
+
+     alert("my sister");
+
+     console.log(dayOfTheMonthPrompt == 27);
+     console.log("my sister");
+
+ }else if (dayOfTheMonthPrompt == 28){
+
+     alert("a baseball bat");
+
+     console.log(dayOfTheMonthPrompt == 28);
+     console.log("a baseball bat");
+
+ }else if (dayOfTheMonthPrompt == 29){
+
+     alert("a dvd player");
+
+     console.log(dayOfTheMonthPrompt == 29);
+     console.log("a dvd player");
+
+ }else if (dayOfTheMonthPrompt == 30){
+
+     alert("my best friends brother");
+
+     console.log(dayOfTheMonthPrompt == 30);
+     console.log("my best friends brother");
+
+ }else if (dayOfTheMonthPrompt == 31){
+
+     alert("my cell phone");
+
+     console.log(dayOfTheMonthPrompt == 31);
+     console.log("my cell phone");
+
+ }else if (dayOfTheMonthPrompt >= 0){
+
+     alert("Aww come on now!");
+     alert("Goodbye");
+
+     for (initialization; condition; increment) {
+         // statements
+     }
+
+
+ }
+
+
+var colorOfShirt = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+ console.log(colorOfShirt);
+ console.log(colorOfShirt.length);
+
+ alert("Pick the number that matches the shirt you are wearing right now.");
+
+ var colorOfShirtPrompt = prompt("What color shirt are you wearing right now?");
+
+ if (colorOfShirtPrompt  == 1) {
+
+     alert("because I was late.");
+
+     console.log(colorOfShirtPrompt == 1);
+     console.log("because I was late.");
+
+ }else if (colorOfShirtPrompt == 2){
+
+     alert("because I was early.");
+
+     console.log(colorOfShirtPrompt == 2);
+     console.log("because I was early.");
+
+ }else if (colorOfShirtPrompt == 3){
+
+     alert("because your mom told me to.");
+
+     console.log(colorOfShirtPrompt == 3);
+     console.log("because your mom told me to.");
+
+ }else if (colorOfShirtPrompt == 4){
+
+     alert("because I want to act silly.");
+
+     console.log(colorOfShirtPrompt == 4);
+     console.log("because I want to act silly.");
+
+ }else if (colorOfShirtPrompt == 5){
+
+     alert("because the frog told me to.");
+
+     console.log(colorOfShirtPrompt == 5);
+     console.log("because the frog told me to.");
+
+ }else if (colorOfShirtPrompt == 6){
+
+     alert("because I do what I want.");
+
+     console.log(colorOfShirtPrompt == 6);
+     console.log("because I do what I want");
+
+ }else if (colorOfShirtPrompt == 7){
+
+     alert("because I love myself.");
+
+     console.log(colorOfShirtPrompt == 7);
+     console.log("because I love myself.");
+
+
+ }else if (colorOfShirtPrompt == 8){
+
+     alert("because I'm shy.");
+
+     console.log(colorOfShirtPrompt == 8);
+     console.log("because I'm shy.");
+
+ }else if (colorOfShirtPrompt == 9){
+
+     alert("because that's how I roll.");
+
+     console.log(colorOfShirtPrompt == 9);
+     console.log("because that's how I roll.");
+
+ }else if (colorOfShirtPrompt == 10){
+
+     alert("because someone offered me a million dollars.");
+
+     console.log(colorOfShirtPrompt == 10);
+     console.log("because someone offered me a million dollars.");
+
+ }else if (colorOfShirtPrompt == 11){
+
+     alert("because I love my family.");
+
+     console.log(colorOfShirtPrompt == 11);
+     console.log("because I love my family.");
+
+ }else if (colorOfShirtPrompt == 12){
+
+     alert("because that's what I do.");
+
+     console.log(colorOfShirtPrompt == 12);
+     console.log("because that's what I do.");
+
+
+ }
+
+
+alert("End of game thanks for playing.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
